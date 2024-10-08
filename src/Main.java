7f07c002ddfc7844e6eda2766b6e03e5c73dbe51
peaksigma <s31142@pjwstk.edu.pl>
2024-10-08 13:31:10 +0200

    Specified who adds the missing parts

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@
 public class Main {
     public static void main(String[] args) {
         Adder adder = new Adder();
-        System.out.println(adder.add(1, 2));
+        System.out.println(adder.add(1, 2,3,4));
 
 
 
